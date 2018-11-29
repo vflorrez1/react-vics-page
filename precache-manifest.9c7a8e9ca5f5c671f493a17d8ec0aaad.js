8e06b2bf802278417563f15ff7e7c823d5a41465
Victor <Victor@Katharines-MBP.localdomain>
2018-11-29 10:05:19 +0000

    Updates

---
+++ precache-manifest.9c7a8e9ca5f5c671f493a17d8ec0aaad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-vics-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3914ffd3abf8740d1c3e",
+    "url": "/react-vics-page/static/js/runtime~main.3914ffd3.js"
+  },
+  {
+    "revision": "98bbfa138ad305a4bb8a",
+    "url": "/react-vics-page/static/js/main.98bbfa13.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-vics-page/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "98bbfa138ad305a4bb8a",
+    "url": "/react-vics-page/static/css/main.66f5f28d.chunk.css"
+  },
+  {
+    "revision": "8d911b2c92a117a33aa63fa7f561688c",
+    "url": "/react-vics-page/index.html"
+  }
+];
